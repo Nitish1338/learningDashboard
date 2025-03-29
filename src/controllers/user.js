2afb276349d0335405f2b732420d7ebc86b3aa6a
Nitish1338 <nitishmahto451@gmail.com>
2025-03-29 16:41:56 +0530

    c

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.js";
 import { uploadCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
 
 
 const generateAccessandRefreshTokens= async(userId)=>{
