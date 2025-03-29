63a4f2bfd7c6083ead47f4359f22641819fabad6
Nitish1338 <nitishmahto451@gmail.com>
2025-03-29 16:16:02 +0530

    c

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -121,8 +121,7 @@ const loginUser = asyncHandler(async(req, res)=> {
     generateAccessandRefreshTokens(user._id)
 
 
-   const loggedInUser = await User.findById(user._id)
-   select("-password -refreshToken")
+   const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
 
    const options = {
